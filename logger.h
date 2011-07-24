8dfb9b9564e0b46f87c80285342d130057696afa
Markus Meissner <meissner@em.cs.uni-frankfurt.de>
2011-07-24 23:00:08 +0200

    more logger stuff, near testing

--- logger.h
+++ logger.h
@@ -9,56 +9,77 @@
 #include "exception.h"
 #include "xstring.h"
 
-#define LOG(lvl)  \
-  if(TOOLS::Logger::instance == NULL) { \
-    std::cerr << "You are trying to log, without having the logger initialized - exiting..." << endl; \
-    exit(1); \
-  } TOOLS::LogStream(TOOLS::Logger::instance,lvl,__LINE__,__FILE__,__FUNCTION__) 
-
-#define DEBUG LOG(10)
-#define INFO LOG(7)
-#define WARN LOG(3)
-#define ERROR LOG(1)
+#define LOG(lvl, logid)  \
+  TOOLS::LogStream(TOOLS::Logger::get(logid), lvl,__LINE__,__FILE__,__FUNCTION__) 
+#define DEBUG \
+    TOOLS::LogStream(TOOLS::Logger::get(logid), 10,__LINE__,__FILE__,__FUNCTION__) 
+#define INFO \
+    TOOLS::LogStream(TOOLS::Logger::get(logid), 7,__LINE__,__FILE__,__FUNCTION__) 
+#define WARN \
+    TOOLS::LogStream(TOOLS::Logger::get(logid), 5,__LINE__,__FILE__,__FUNCTION__) 
+#define ERROR \
+    TOOLS::LogStream(TOOLS::Logger::get(logid), 3,__LINE__,__FILE__,__FUNCTION__) 
 
 namespace TOOLS {
 
   DEFINE_EXCEPTION(BackendFailedToWrite, BaseException);
-  
+  DEFINE_EXCEPTION(NoSuchLoggerAvailable, BaseException);
+
+  class Logger;
+
+  typedef std::map<std::string, Logger*> tLoggerMap;
+
   class BaseLoggerBackend {
     public:
       virtual void write(const std::string& text) = 0;
       virtual void init_sink() = 0;
       virtual void cleanup_sink() = 0;
-  };
+  }; // must they be pure virtual, (*sink)
 
   class FileBackend : public BaseLoggerBackend {
     public:
-      void write(const std::string&);
+      std::string filename;
+
+      FileBackend(const std::string& fn);
+
+      void write(const std::string& msg);
   };
-  //class Persistent
+  //class PersistentFileBackend
+
+  class ConsoleBackend : public BaseLoggerBackend {
+    //public:
+      //void 
+      // write console backend, and think about multiple backends inside logger
+  }
 
   
   class Logger {
     public:
-      Logger(BaseLoggerBackend* backend);
+      Logger(const std::string& id, BaseLoggerBackend* back);
 
-      static Logger* instance;
+      static tLoggerMap log_map;
+      static Logger* get(const std::string& id) throw (NoSuchLoggerAvailable);
 
       void set_logging_template(const std::string& tmpl);
       void set_time_format(const std::string& format);
 
-      void log_msg(const std::string& data);
+      void log_msg(std::string& data, int loglevel, std::string& line, const std::string& fn, const std::string& func);
 
     private:
       BaseLoggerBackend* backend;
-      string log_template;
-      string time_format;
+      std::string log_template;
+      std::string time_format;
   };
-
-  Logger* Logger::instance = NULL;
+  tLoggerMap Logger::log_map;
 
   class LogStream : public std::ostringstream {
     public:
+      Logger* obj;
+      int loglvl;
+      std::string line;
+      std::string fn;
+      std::string func;
+
       LogStream(Logger* logobj, int loglvl, const std::string& line, const std::string& fn, const std::string& func);
       ~LogStream();
   };
