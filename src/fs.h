c5dc4739fca2fea99f041af7eca4a4fd4bbbf7a2
Markus Meissner <meissner@em.cs.uni-frankfurt.de>
2014-09-14 08:49:00 +0200

    some planing inside fs.h

--- src/fs.h
+++ src/fs.h
@@ -31,6 +31,20 @@ DEFINE_EXCEPTION(PathException, BaseException)
  */
 class Path {
  public:
+  typedef enum {
+    RIGHTS_UNSET = 0,
+    RIGHTS_OTHER = 1,
+    RIGHTS_GROUP = 2,
+    RIGHTS_USER = 4
+  } RIGHTS_PERSON;
+
+  typedef enum {
+    MODE_UNSET = 0,
+    MODE_EXEC = 1,
+    MODE_READ = 2,
+    MODE_WRITE = 4
+  } RIGHTS_MODE;
+
   std::string path;
 
   Path();
@@ -38,6 +52,7 @@ class Path {
   Path(const Path& obj);
   ~Path();
 
+  // forwarders to underlying string
   bool empty() const;
   size_t size() const;
   const char* c_str() const;
@@ -46,17 +61,37 @@ class Path {
   bool is_dir();
   bool is_file();
   //bool is_symlink();
+  //bool is_socket();
+
+  //int get_uid();
+  //string get_user();
+  //int get_gid();
+  //string get_group();
+
+  //RIGHTS_MODE get_mode(const RIGHTS_PERSON& who);
+
+  //bool is_readable();
+  //bool is_writeable();
+  //bool is_executable();
+  
+  //
+  //bool touch();
+
+
 
   // dirname
   Path parent();
+  //Path dirname(); -- make alias
 
   // basename
   Path pwd();
-
+  //Path basename(); -- make alias
+  
   Path join(const std::string& what);
 
-  // bool recursive missing
+  // bool TODO: recursive missing
   bool create_dir();
+  //bool mkdir(bool recursive); -- make alias
 
   bool move(const std::string& to);
   
@@ -64,6 +99,7 @@ class Path {
 
   //bool copy(bool recursive, Path target);
 
+  // func(const std::string& dir, const std::string& fn)
   //void walk(lambda func);
 
   std::vector<Path> listdir(bool recursive=false);
@@ -75,11 +111,11 @@ class Path {
   bool operator==(const Path& obj);
   Path& operator=(const std::string& obj);
 
-  //operator std::string();
 };
 
 // add Path overloaded
 bool change_dir(const std::string& path);
+//bool change_dir(const FS::Path& path);
 std::string current_dir();
 
 Path operator+(const char* lhs, Path& rhs);
