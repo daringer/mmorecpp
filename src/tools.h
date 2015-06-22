ed29d10fca15c8d1b020c23046287420a7021ed9
Markus Meissner <coder@safemailbox.de>
2015-06-22 14:26:45 +0200

    tools.h remove junk

--- src/tools.h
+++ src/tools.h
@@ -1,11 +1,5 @@
 #pragma once 
 
-
-// An include-include file to ease 'mmorecpp's inclusion:
-
-// core is always included, but may be excluded
-//#ifndef NO_CORE
-
 #include "core/general.h"
 #include "core/exception.h"
 #include "core/converter.h"
@@ -14,19 +8,11 @@
 #include "core/xtime.h"
 #include "core/xunit.h"
 
-//#endif
-
-//#ifdef OOAPI
-
 #include "ooapi/config_manager.h"
 #include "ooapi/template_parser.h"
 #include "ooapi/testing_suite.h"
 #include "ooapi/xregex.h"
 #include "ooapi/xsqlite.h"
-
-//#endif 
-
-//#ifdef PLATFORM
 
 #include "platform/executor.h"
 #include "platform/fs.h"
@@ -34,8 +20,3 @@
 #include "platform/xlogger.h"
 #include "platform/xsocket.h"
 
-//#endif 
-
-//#ifdef experimental
-//#include "experimental/xncurses.h"
-//#endif
